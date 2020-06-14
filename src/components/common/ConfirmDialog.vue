7b02e63e9d2136324d147acd5e7a813a6dffc10e
mhmbillah <moh.hasan.muktasyim.billah@gmail.com>
2020-06-15 06:57:17 +0700

    Create ConfirmDialog.vue

---
+++ src/components/common/ConfirmDialog.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-dialog v-model="show" @click:outside="cancel" max-width="500">
+    <v-card>
+      <v-card-title
+        ><span class="title">{{ title }}</span></v-card-title
+      >
+      <v-card-text>
+        <h4>{{ message }}</h4>
+        <v-card-actions>
+          <v-btn
+            color="primary"
+            outlined
+            dark
+            class="mb-2"
+            width="50%"
+            @click="cancel"
+            >Batal</v-btn
+          >
+          <v-btn color="primary" dark class="mb-2" @click="confirm" width="50%"
+            >Ya</v-btn
+          >
+        </v-card-actions>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    message: {
+      type: String,
+      required: true
+    },
+    visible: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    confirm() {
+      this.$emit("confirm");
+    },
+    cancel() {
+      this.$emit("cancel");
+    }
+  },
+  computed: {
+    show: {
+      get() {
+        return this.visible;
+      },
+      set(value) {
+        if (!value) this.$emit("cancel");
+      }
+    }
+  }
+};
+</script>
