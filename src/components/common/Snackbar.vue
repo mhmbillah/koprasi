6088aa1714900ba8654046fa13f1ec3721c48200
mhmbillah <moh.hasan.muktasyim.billah@gmail.com>
2020-06-15 06:57:23 +0700

    Create Snackbar.vue

---
+++ src/components/common/Snackbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-snackbar :timeout="timeout" top :color="color" v-model="visibleSnackbar">
+    {{ text }}
+    <slot></slot>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: "info"
+    },
+    timeout: {
+      type: Number,
+      default: 3000
+    },
+    model: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    visibleSnackbar: {
+      get() {
+        return this.model;
+      },
+      set(value) {
+        if (!value) this.$emit("close");
+      }
+    }
+  }
+};
+</script>
