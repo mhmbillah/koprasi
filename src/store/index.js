2a8393047f0c4a256d29df2f0856e2d9c7fff220
mhmbillah <moh.hasan.muktasyim.billah@gmail.com>
2020-06-14 15:05:46 +0700

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,19 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import category from "./modules/category";
+import anggota from "./modules/anggota";
+import cicilan from "./modules/cicilan";
+import pinjaman from "./modules/pinjaman";
+import simpanan from "./modules/simpanan";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    category
+    category,
+    anggota,
+    cicilan,
+    pinjaman,
+    simpanan
   }
 });
