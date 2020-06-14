dbbbc80114d3db4fbc5888ec1be99e2fbbd51f69
mhmbillah <moh.hasan.muktasyim.billah@gmail.com>
2020-06-15 06:57:14 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,83 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+  <v-app id="keep">
+    <v-app-bar app clipped-left color="#0095da">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <span class="title ml-3 mr-5"
+        >Google&nbsp;<span class="font-weight-light">Keep</span></span
+      >
+      <v-text-field
+        solo-inverted
+        flat
+        hide-details
+        label="Search"
+        prepend-inner-icon="mdi-search"
+      ></v-text-field>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld />
-    </v-content>
+    <v-navigation-drawer v-model="drawer" app clipped color="grey lighten-4">
+      <v-list dense class="grey lighten-4">
+        <template v-for="(item, i) in items">
+          <v-row v-if="item.heading" :key="i" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-right">
+              <v-btn small text>edit</v-btn>
+            </v-col>
+          </v-row>
+          <v-divider
+            v-else-if="item.divider"
+            :key="i"
+            dark
+            class="my-4"
+          ></v-divider>
+          <v-list-item v-else :key="i" link :to="item.to">
+            <v-list-item-action>
+              <v-icon>mdi-{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title class="grey--text">
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid class="grey lighten-4 fill-height">
+        <router-view></router-view>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
-
   data: () => ({
-    //
+    drawer: null,
+    items: [
+      { icon: "cash-plus", text: "Simpanan", to: "/simpanan" },
+      { icon: "cash-minus", text: "Pinjaman", to: "/" },
+      { icon: "cash-refund", text: "Cicilan", to: "/" },
+      { icon: "account-cash", text: "Anggota", to: "/" },
+      { divider: true },
+      { heading: "Labels" },
+      { icon: "add", text: "Create new label" },
+      { divider: true }
+    ]
   })
 };
 </script>
+
+<style>
+/* #keep .v-navigation-drawer__border {
+  display: none;
+} */
+</style>
