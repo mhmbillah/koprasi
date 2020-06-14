3664cdb3b4592927576d6cd7780fa66bd0820d9d
mhmbillah <moh.hasan.muktasyim.billah@gmail.com>
2020-06-14 15:05:30 +0700

    create pinjaman store

---
+++ src/store/modules/pinjaman/actions.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+import api from "../simpanan/node_modules/@/api";
+
+export default {
+  addPinjaman({ commit }, { data, succes, fail } = {}) {
+    api.pinjaman
+      .add(data)
+      .then(response => {
+        succes && succes(response);
+      })
+      .catch(fail);
+  },
+  editPinjaman({ commit }, { data, succes, fail } = {}) {
+    api.pinjaman
+      .edit(data)
+      .then(response => {
+        succes && succes(response);
+      })
+      .catch(fail);
+  },
+  deletePinjaman({ commit }, { data, succes, fail } = {}) {
+    api.pinjaman
+      .delete(data)
+      .then(response => {
+        succes && succes(response);
+      })
+      .catch(fail);
+  },
+  getDetailPinjaman({ commit }, { data, succes, fail } = {}) {
+    api.pinjaman
+      .getDetail(data)
+      .then(response => {
+        succes && succes(response);
+      })
+      .catch(fail);
+  },
+  getPinjaman({ commit }, { success, fail } = {}) {
+    api.pinjaman
+      .get()
+      .then(response => {
+        success && success(response);
+      })
+      .catch(fail);
+  }
+};
