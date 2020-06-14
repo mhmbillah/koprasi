a7fadc8dc9e6452ad1f323eb1963eaf2cd7865be
mhmbillah <moh.hasan.muktasyim.billah@gmail.com>
2020-06-15 06:57:20 +0700

    Create Loading.vue

---
+++ src/components/common/Loading.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-center">
+    <v-progress-circular
+      :size="50"
+      :width="5"
+      color="#0f9cdb"
+      indeterminate
+    ></v-progress-circular>
+  </div>
+</template>
