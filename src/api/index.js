125da1d72558ced30b75fd9452ae3aa599b570cb
mhmbillah <moh.hasan.muktasyim.billah@gmail.com>
2020-06-17 03:13:32 +0700

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,11 +3,13 @@ import anggota from "./modules/anggota";
 import cicilan from "./modules/cicilan";
 import simpanan from "./modules/simpanan";
 import pinjaman from "./modules/pinjaman";
+import kas from "./modules/kas";
 
 export default {
   // category,
   anggota,
   cicilan,
   simpanan,
-  pinjaman
+  pinjaman,
+  kas
 };
