4d91bc30f4a92ad9002facadc710533ee45ed4ce
mhmbillah <moh.hasan.muktasyim.billah@gmail.com>
2020-06-17 03:13:28 +0700

    Update routes.js

--- src/api-mock/routes.js
+++ src/api-mock/routes.js
@@ -3,7 +3,8 @@ import anggota from "./modules/anggota";
 import cicilan from "./modules/cicilan";
 import pinjaman from "./modules/pinjaman";
 import simpanan from "./modules/simpanan";
+import kas from "./modules/kas";
 
-const routes = [...anggota, ...simpanan, ...pinjaman, ...cicilan];
+const routes = [...anggota, ...simpanan, ...pinjaman, ...cicilan, ...kas];
 
 export default routes;
